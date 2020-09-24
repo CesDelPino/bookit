cea4ddb2cca7e6ee84a11ca54d2e748d31e9e63d
Cesar Del Pino <cesar@delpino.com>
2020-09-25 01:34:16 +1000

    working ARGV for app help instructions

--- lib/bookit.rb
+++ lib/bookit.rb
@@ -1,7 +1,35 @@
-require "bookit/version"
+require_relative "bookit/version"
+require_relative "options"
 
 module Bookit
   class Error < StandardError; 
   end
   # Your code goes here...
 end
+
+def help_menu(option)
+  case 
+  when option == "-h" || "--help" || "help"
+    puts "this is the help menu"
+    exit
+  else
+    puts "That is an unknown command, you can try the following......"
+    exit
+  end
+
+end
+
+
+if ARGV.empty?
+  puts "Welcome to the bookit app"
+  caller = OpeningOptions.new
+  caller.openining_choice
+elsif
+  option = ARGV[0]
+  ARGV.clear
+  puts "You have selected #{option}"
+  help_menu(option)
+else
+end
+
+

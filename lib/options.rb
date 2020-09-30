d14c8b5c27560dedbe0b5374b1c22de85bf8d7a0
Cesar Del Pino <cesar@delpino.com>
2020-10-01 09:40:55 +1000

    options handles the main routing loop and initiates reading of database

--- lib/options.rb
+++ lib/options.rb
@@ -1,39 +1,52 @@
- class OpeningOptions
-  
+require_relative "reservation"
+require_relative "reservation_handler"
+
+class OpeningOptions
+  def initialize
+    @reservation = ReservationHandler.new
+  end
+
   def selection
     choice = gets.chomp
   end
- 
 
   def show_options
-    puts "Please select from the following options"
-    puts "to see todays availability select 1"
-    puts "to make a booking for today select 2"
-    puts "to modify of cancel a booking select 3"
-    puts "to display all bookings select 4"
+    puts "\n"
+    puts "   Please select from the following options    "
+    puts "\n"
+    puts "**>To see todays availability select 1".colorize(:green)
+    puts "**>To make a booking for today select 2".colorize(:green)
+    puts "**>To modify a booking select 3".colorize(:green)
+    puts "**>To ".colorize(:green)+"DELETE".colorize(:red) + " a booking select 4".colorize(:green)
+    puts "**>To display all bookings select 5".colorize(:green)
+    puts "**>To EXIT and SAVE changes select 6".colorize(:green)
     print "-->"
   end
 
-
-
-  def openining_choice
+  def opening_choice
     loop do
       show_options
       case selection
-      when "1"
-        puts "Lets see the availability"
-        exit
+      when "1"       
+        @reservation.find_open_times
+        @reservation.free_times_printer
       when "2"
-        puts "lets make a booking"
-        exit
-      when "3"
-        puts "lets modify/cancel"
+        @reservation.find_open_times
+        @reservation.slot_checker
+      when "3" 
+        @reservation.modify_booking
       when "4"
-        puts "display all bookings"
+        @reservation.delete_booking
+      when "5"
+        @reservation.display_all
+      when "6"
+        @reservation.save_changes
+        exit
       else 
-        "puts invalid selection"
+        puts "invalid selection".colorize(:red)
       end
     end
   end
+
 end
 

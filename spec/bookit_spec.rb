eb951501c85687721b6c3d7f806ddaf8931e64bd
Cesar Del Pino <cesar@delpino.com>
2020-10-04 10:23:02 +1100

    trying testing

--- spec/bookit_spec.rb
+++ spec/bookit_spec.rb
@@ -1,9 +1,13 @@
-RSpec.describe Bookit do
-  it "has a version number" do
-    expect(Bookit::VERSION).not_to be nil
-  end
+# require_relative '../lib/reservation_handler'
+# require_relative '../lib/bookit'
+# require_relative '../lib/bookit/version'
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
+# RSpec.describe Bookit do
+#   it "has a version number" do
+#     expect(Bookit::VERSION).not_to be nil
+#   end
+
+#   it "does something useful" do
+#     expect(false).to eq(true)
+#   end
+# end

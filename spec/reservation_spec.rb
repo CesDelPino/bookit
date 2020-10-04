c417a425898be845058a66b7ac772ede720fa04b
Cesar Del Pino <cesar@delpino.com>
2020-10-04 11:44:37 +1100

    finally a working test

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -2,8 +2,9 @@ require_relative '../lib/reservation'
 
 describe Reservation do
   it "Should isntanciate data" do
-    sample_booking = Reservation.new('12','Jenny', "0400320070", 'taking grandma')
-    expect (sample_booking.name).to_eq 'Jenny'
-    expect (sample_booking).to be an_instance_of Reservation
+    sample_booking = Reservation.new('12','Jenny', "0423320070", 'nil')
+    expect (sample_booking.notes) == ('nil')
+    expect (sample_booking.name) == ('Jenny')
   end
 end
+
